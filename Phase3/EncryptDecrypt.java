a084683ca5e3a7c536fd3dee57558383ae022df8
coreyrau <crau1012@gmail.com>
2017-03-06 03:28:37 -0800

    Add files via upload

--- Phase3/EncryptDecrypt.java
+++ Phase3/EncryptDecrypt.java
@@ -10,8 +10,8 @@ import org.json.*;
 
 /**
  * 
- * @author Corey
- * Encrypt and Decyrpt
+ * @author Corey and Anthony
+ * Encrypt and Decrypt
  */
 public class EncryptDecrypt {
 	
@@ -33,22 +33,26 @@ public class EncryptDecrypt {
 		String input = "Text to be encrypted";
 		
 		JSONObject json = Encrypt(input, Public_Key_File);
+		String plainText = Decrypt(json, Private_Key_File);
+		
+		System.out.println(plainText);
 	}
 	
 	/**
 	 * 
 	 * @param text - plain text to be encrypted
-	 * @param pubKey - public rsakey
-	 * @return ciphertext
+	 * @param file - file path of the public rsakey
+	 * @return JSONObject - contains aesCipherText, rsaCipherText, HMAC Tag, and IV
 	 */
 	public static JSONObject Encrypt(String text, String file) {
 		
+		//rsa ciphertext - aes key and hmac key concatenated and then rsa encrypted
 		byte[] rsaCipherText = null;
+		//encrypted plain text
 		byte[] aesCipherText = null;
 		byte[] hmacTag = null;
 		JSONObject jsonObject = new JSONObject();
 		
-		//generates the aes and hmac keys
 		try {
 			
 			//get public key from file and create rsa object
@@ -63,8 +67,9 @@ public class EncryptDecrypt {
 			keyGen.init(256);
 			SecretKey aesKey = keyGen.generateKey();
 			
-			//create the aes iv
+			//creates the aes iv
 			byte[] iv = new byte[16];
+			//random bytes for the IV
 			SecureRandom rng = new SecureRandom();
 			rng.nextBytes(iv);
 			IvParameterSpec IV =  new IvParameterSpec(iv);
@@ -76,8 +81,6 @@ public class EncryptDecrypt {
 			
 			//encrypt the text with the aes key
 			aesCipherText = aesCipher.doFinal(text.getBytes());
-			//convert from binary to hex string
-			String aesText = Base64.getEncoder().encodeToString(aesCipherText);
 
 			//generate the HMACSha-256bit key
 			byte[] hmacArray = new byte[32];
@@ -86,7 +89,6 @@ public class EncryptDecrypt {
 			SecretKeySpec hmacsecKey = new SecretKeySpec(hmacArray, "HmacSHA256");
 			hmackey.init(hmacsecKey);
 			hmacTag = hmackey.doFinal(aesCipherText);
-			String hmacFinal = Base64.getEncoder().encodeToString(hmacTag);
 			
 			//concatenate the aes and hmac keys
 			byte[] aesKeyByte = aesKey.getEncoded();
@@ -103,34 +105,23 @@ public class EncryptDecrypt {
 			jsonObject.put("AES IV", DatatypeConverter.printHexBinary(IV.getIV()));
 			jsonObject.put("HMAC Tag", DatatypeConverter.printHexBinary(hmacTag));
 			
-			System.out.println("RSA CipherText: " + DatatypeConverter.printHexBinary(rsaCipherText));
-			System.out.println("AES CipherText: " + DatatypeConverter.printHexBinary(aesCipherText));
-			System.out.println("AES IV: " + DatatypeConverter.printHexBinary(IV.getIV()));
-			System.out.println("HMAC Tag: " + DatatypeConverter.printHexBinary(hmacTag));
+			//print out jsonObject
+			System.out.println(jsonObject.toString());
 			
-		} catch (NoSuchAlgorithmException e) {
-			System.out.println("Algorithm not found");
-		} catch (NoSuchPaddingException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-		} catch (InvalidKeyException e) {
-			e.printStackTrace();
-		} catch (InvalidAlgorithmParameterException e) {
-			e.printStackTrace();
-		} catch (IllegalBlockSizeException e) {
-			e.printStackTrace();
-		} catch (BadPaddingException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (InvalidKeySpecException e) {
-			e.printStackTrace();
-		} catch (JSONException e) {
-			e.printStackTrace();
-		} 
+		}
 		return jsonObject;
 	}
 	
-	public static String Decrypt(JSONObject jsonObJect, String file) {
+	/**
+	 * 
+	 * @param jsonObject - contains aesCipherText, rsaCipherText, HMAC Tag, and IV from encryption
+	 * @param file - path location of the private rsa key
+	 * @return - decrypted plaintext
+	 */
+	public static String Decrypt(JSONObject jsonObject, String file) {
+		
 		String plainText = null;
 		
 		try {
@@ -138,15 +129,52 @@ public class EncryptDecrypt {
 			byte[] keyBytes = Files.readAllBytes(new File(file).toPath());
 			PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
 			KeyFactory kf = KeyFactory.getInstance("RSA");
-			Cipher rsaCipher = Cipher.getInstance("RSA/ECB//OAEPWithSHA-256MGF1Padding");
+			Cipher rsaCipher = Cipher.getInstance("RSA/ECB//OAEPWithSHA-256andMGF1Padding");
 			rsaCipher.init(Cipher.DECRYPT_MODE, kf.generatePrivate(spec));
 			
-			//recover aesCipherText
-			//String aesFromJson = JSONObject.getString("AES CipherText");
+			//recover rsa cipher text
+			String rsa = jsonObject.getString("RSA CipherText");
+			byte[] rsaCipherText = DatatypeConverter.parseHexBinary(rsa);
+			
+			//Decrypt concatenated keys
+			byte[] aesHMACKeys = rsaCipher.doFinal(rsaCipherText);
+			
+			//get aes key and hmackey from concatenated key
+			byte[] aesKey = new byte[aesHMACKeys.length / 2];
+			byte[] hmacKey = new byte[aesHMACKeys.length / 2];
+			System.arraycopy(aesHMACKeys, 0, aesKey, 0, aesKey.length);
+			System.arraycopy(aesHMACKeys, aesKey.length, hmacKey, 0, hmacKey.length);
+			
+			//recover aesCipherText and IV
+			String aesFromJson = jsonObject.getString("AES CipherText");
+			byte[] aesCipherText = DatatypeConverter.parseHexBinary(aesFromJson);
+			String iv = jsonObject.get("AES IV").toString();
+			byte[] IV = DatatypeConverter.parseHexBinary(iv);
+			
+			//recover HMACTag
+			String HMAC = jsonObject.getString("HMAC Tag");
+			byte[] hmacTag1 = DatatypeConverter.parseHexBinary(HMAC);
+			
+			//Generate an HMACTag
+			Mac hmackey = Mac.getInstance("HmacSHA256");
+			SecretKeySpec hmacsecKey = new SecretKeySpec(hmacKey, "HmacSHA256");
+			hmackey.init(hmacsecKey);
+			byte[] hmacTag2 = hmackey.doFinal(aesCipherText);
+			
+			//compare hmac tags
+			if (java.util.Arrays.equals(hmacTag1, hmacTag2)) {
+				
+				//create aes cipher object and decrypt message
+				Cipher aesCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+				aesCipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(aesKey, "AES"), new IvParameterSpec(IV));
+				plainText = new String (aesCipher.doFinal(aesCipherText), "US-ASCII");
+		
+			} else {
+				System.out.println("HMACTags did not match, Decryption  Failure");
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 		return plainText;
 	}
 }
